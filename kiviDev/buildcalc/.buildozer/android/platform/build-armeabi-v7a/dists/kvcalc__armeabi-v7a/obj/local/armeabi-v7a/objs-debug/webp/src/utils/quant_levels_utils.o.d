7541c381c6f9380a9b4682d6375825168c10dca7
George Kent-Scheller <georgeks@bu.edu>
2021-11-30 14:29:39 -0500

    I got rid of the 2 gig lib that was part of build dowser

--- kiviDev/buildcalc/.buildozer/android/platform/build-armeabi-v7a/dists/kvcalc__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/webp/src/utils/quant_levels_utils.o.d
+++
@@ -1,8 +0,0 @@
-/home/whorehay/Desktop/github/pythnScripts/kiviDev/buildcalc/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/utils/quant_levels_utils.o: \
-  /home/whorehay/Desktop/github/pythnScripts/kiviDev/buildcalc/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/quant_levels_utils.c \
-  /home/whorehay/Desktop/github/pythnScripts/kiviDev/buildcalc/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/quant_levels_utils.h \
-  /home/whorehay/Desktop/github/pythnScripts/kiviDev/buildcalc/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/types.h
-
-/home/whorehay/Desktop/github/pythnScripts/kiviDev/buildcalc/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/quant_levels_utils.h:
-
-/home/whorehay/Desktop/github/pythnScripts/kiviDev/buildcalc/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/types.h:

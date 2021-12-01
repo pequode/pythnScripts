9631071dd77a3e63bdace2ebde20e92c771a153d
George Kent-Scheller <georgeks@bu.edu>
2021-12-01 00:38:31 -0500

    naming fixes

--- CV Stuff/ColorTrackingPoints/pointTester.py
+++
@@ -1,3 +0,0 @@
-import point
-pnt = point.Point(2,3)
-print(pnt.x)
--- CV Stuff/test for oop.py
+++
@@ -1,9 +0,0 @@
-class kent:
-    
-    kill ="death"
-    def __init__(self,kll = None):
-        kill = kll
-    def prnt():
-        print(kill)
-k = kent("dead")
-k.prnt()
